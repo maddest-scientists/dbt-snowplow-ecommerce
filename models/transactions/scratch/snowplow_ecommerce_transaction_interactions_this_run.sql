093ee4d678f5b44eb758729532675b15419b08cb
Razvan <paul_avram@smart-x.net>
2023-06-12 12:28:31 +0300

    Mitigate duplicated transaction ids

--- models/transactions/scratch/snowplow_ecommerce_transaction_interactions_this_run.sql
+++ models/transactions/scratch/snowplow_ecommerce_transaction_interactions_this_run.sql
@@ -5,7 +5,41 @@
     )
 }}
 
-with transaction_info AS (
+with unique_transactions as (
+    select
+        e.event_id,
+        e.page_view_id,
+
+        e.domain_sessionid,
+        e.event_in_session_index,
+
+        e.domain_userid,
+        e.network_userid,
+        e.user_id,
+        e.ecommerce_user_id,
+
+        e.derived_tstamp,
+        DATE(e.derived_tstamp) as derived_tstamp_date,
+
+        e.transaction_id as transaction_id,
+        e.transaction_currency as transaction_currency,
+        e.transaction_payment_method,
+        e.transaction_revenue,
+        e.transaction_total_quantity,
+        e.transaction_credit_order,
+        e.transaction_discount_amount,
+        e.transaction_discount_code,
+        e.transaction_shipping,
+        e.transaction_tax,
+
+        e.ecommerce_user_email,
+        e.ecommerce_user_is_guest
+    from {{ ref('snowplow_ecommerce_base_events_this_run') }} as e
+    where ecommerce_action_type = 'transaction'
+    qualify row_number() over (partition by transaction_id order by derived_tstamp) = 1
+),
+
+transaction_info AS (
     select
         -- event fields
         e.event_id,
@@ -46,12 +80,10 @@ with transaction_info AS (
         {%- else -%}
         COUNT(*) as number_products -- count all products added
         {%- endif %}
-
-    from {{ ref('snowplow_ecommerce_base_events_this_run') }} as e
+    from unique_transactions as e
     {% if not var("snowplow__disable_ecommerce_products", false)  -%}
     left join {{ ref('snowplow_ecommerce_product_interactions_this_run') }} as pi on e.transaction_id = pi.transaction_id AND pi.is_product_transaction
     {%- endif %}
-    where ecommerce_action_type = 'transaction'
     group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22
 )
 
